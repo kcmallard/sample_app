5cf3296e22457e59c1d5fad8cc1a7f03ff0895ae
Kyle Mallard <kylemallard@gmail.com>
2014-09-17 06:51:23 -0500

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
   gem 'spork-rails', '4.0.0'
+  gem 'cucumber-rails', '1.4.0', :require => false
+  gem 'database_cleaner', github: 'bmabey/database_cleaner'
   gem 'guard-spork', '1.5.0'
   gem 'childprocess', '0.3.6'
   gem 'factory_girl_rails', '4.2.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,14 @@ describe User do
 
 	subject { @user }
 
+	describe "remember token" do 
+  		before { @user.save }
+  		its(:remember_token) { should_not be_blank }
+  	end
+	
+	it { should respond_to(:password_confirmation) }
+  	it { should respond_to(:remember_token) }
+  	it { should respond_to(:authenticate) }
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
